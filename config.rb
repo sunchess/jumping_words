82675abc63fe6e65e788cc06b378e4678b509f7d
Александр Дмитриев <alex@devmen.com>
2015-03-14 14:38:59 +0300

    added repeat words

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ DATABASE = "jumping_words_db"
 # Database directory
 DATABASE_DIR = ".jwords"
 # space between word and translate
-SPACE_WORDS = 3
+SPACE_WORDS = 2
 # space iteration
 SPACE_INTERATION = 10
 #show message box true or false
@@ -12,3 +12,5 @@ SHOW_MESSAGE = true
 MAC_WORD_VOICE = 'Vicki'
 #MAC OS X translate voice
 MAC_TRANSLATE_VOICE = 'Milena'
+#how many times repeat each word in the cycle
+REPEAT = 1
--- lib/jwords/say_words.rb
+++ lib/jwords/say_words.rb
@@ -9,6 +9,7 @@ class SayWords
     @space_interation = options[:space_interation] || SPACE_INTERATION
     @mac_word_voice = options[:mac_word_voice] || MAC_WORD_VOICE
     @mac_translate_voice = options[:mac_translate_voice] || MAC_TRANSLATE_VOICE
+    @repeat = options[:repeat] || REPEAT
   end
 
   def run
@@ -23,19 +24,27 @@ class SayWords
   def do_ubuntu
     loop do
       @collection.each_with_index do |word, idx|
-        puts "#{word[0]}: #{word[1]} | #{word[2]}"
-        system 'notify-send -i /usr/share/pixmaps/gnome-irc.png "'+ word[0] +'" "'+ word[1] +'"  -t 1' if @show_message
-        system 'espeak -v en -s120 "'+ word[0] +'"'
-        sleep @space_words
-        #puts "I say #{word[1]}"
-        system 'espeak -v ru -s120 "'+ word[1] +'"'
+        rep_times = !word[2].nil? ? word[2].to_i : 1
+        @repeat.times do
 
-        #set number of showing
-        @db.update(idx, [word[0], word[1], !word[2].nil? ? word[2].to_i + 1 : 1])
+          rep_times += 1
 
+          puts "#{word[0]}: #{word[1]} | #{rep_times}"
+          system 'notify-send -i /usr/share/pixmaps/gnome-irc.png "'+ word[0] +'" "'+ word[1] +'"  -t 1' if @show_message
+          system 'espeak -v en -s120 "'+ word[0] +'"'
+          sleep @space_words
+
+          system 'espeak -v ru -s120 "'+ word[1] +'"'
+
+          #set number of showing
+          @db.update(idx, [word[0], word[1], rep_times])
+
+          sleep @space_words
+        end
         sleep @space_interation
       end
       sleep 4 # for notification hiding
+
       #cleaning
       GC.start
     end
@@ -44,17 +53,25 @@ class SayWords
   def do_mac
     loop do
       @collection.each_with_index do |word, idx|
-        puts "#{word[0]}: #{word[1]} | #{!word[2].nil? ? word[2].to_i + 1 : 1}"
-        #puts 'terminal-notifier -message "'+ word[0] +': '+ word[1] +'" -title "JWords"' if @show_message
-        system 'terminal-notifier -message "'+ word[0] +': '+ word[1] +'" -title ""' if @show_message
-        system 'say -v ' + @mac_word_voice + ' "'+ word[0] +'"'
-        sleep @space_words
-        #puts "I say #{word[1]}"
-        system 'say -v '+ @mac_translate_voice +' "'+ word[1] +'"'
+        rep_times = !word[2].nil? ? word[2].to_i : 1
+        @repeat.times do
 
-        #set number of showing
-        @db.update(idx, [word[0], word[1], !word[2].nil? ? word[2].to_i + 1 : 1])
+          rep_times += 1
 
+          puts "#{word[0]}: #{word[1]} | #{rep_times}"
+
+          system 'terminal-notifier -message "'+ word[0] +': '+ word[1] +'" -title ""' if @show_message
+          system 'say -v ' + @mac_word_voice + ' "'+ word[0] +'"'
+
+          sleep @space_words
+
+          system 'say -v '+ @mac_translate_voice +' "'+ word[1] +'"'
+
+          #set number of showing
+          @db.update(idx, [word[0], word[1], rep_times])
+
+          sleep @space_words
+        end
         sleep @space_interation
       end
       sleep 4 # for notification hiding
